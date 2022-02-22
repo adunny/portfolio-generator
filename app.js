e8b7501257f74853f52be1685ba683dc1ccea48d
alen <alen.dunn1@gmail.com>
2022-02-22 17:39:11 -0500

    issue 2 resolved

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log("============")
+const [profileName, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+
+fs.writeFile('./index.html', generatePage(profileName, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio Complete! Check out index.html to see the output!')
+});
+
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log("============")
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
